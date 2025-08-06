97eb401cfab512b95565149efe0720821512107d
peterdinis <petter1@pobox.sk>
2025-08-06 21:07:52 +0200

    feat: New column to page table

---
+++ prisma/migrations/20250806190419_add_new_column_to_page_table/migration.sql
@@ -0,0 +1,22 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Page" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "isDatabase" BOOLEAN NOT NULL DEFAULT false,
+    "parentPageId" INTEGER,
+    "workspaceId" INTEGER NOT NULL,
+    "sortPosition" INTEGER NOT NULL DEFAULT 0,
+    "ownerId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Page_parentPageId_fkey" FOREIGN KEY ("parentPageId") REFERENCES "Page" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Page_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Page_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Page" ("createdAt", "id", "isDatabase", "ownerId", "parentPageId", "title", "updatedAt", "workspaceId") SELECT "createdAt", "id", "isDatabase", "ownerId", "parentPageId", "title", "updatedAt", "workspaceId" FROM "Page";
+DROP TABLE "Page";
+ALTER TABLE "new_Page" RENAME TO "Page";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
