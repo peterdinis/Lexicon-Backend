ebcbe617bcf54a87f259f04f9637e5f7dda4cf55
peterdinis <petter1@pobox.sk>
2025-08-07 15:29:54 +0200

    feat: Working on tastks module WIP

---
+++ src/tasks/dto/create-task.input.ts
@@ -0,0 +1,33 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsOptional, IsString, IsEnum, IsDateString, IsInt } from 'class-validator';
+import { TaskStatus } from 'generated/prisma';
+
+@InputType()
+export class CreateTaskInput {
+  @Field()
+  @IsString()
+  title: string;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @Field(() => TaskStatus, { defaultValue: TaskStatus.todo })
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsDateString()
+  dueDate?: string;
+
+  @Field(() => Number)
+  @IsInt()
+  pageId: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  @IsInt()
+  assigneeId?: number;
+}
