2c7db8892b0333d3340f7ee535773c240cbecc69
peterdinis <petter1@pobox.sk>
2025-08-07 18:53:33 +0200

    FOO

---
+++ src/shared/plugins/logging.plugin.ts
@@ -0,0 +1,15 @@
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+
+export function LoggingPlugin(): ApolloServerPlugin {
+  return {
+    async requestDidStart(requestContext) {
+      console.log('GraphQL Request started', requestContext.request.operationName);
+
+      return {
+        async willSendResponse(responseContext) {
+          console.log('GraphQL Response sent for', responseContext.request.operationName);
+        },
+      };
+    },
+  };
+}
