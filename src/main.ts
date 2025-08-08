bd1893d3089625d603aaf86f152bbec56a10a679
peterdinis <petter1@pobox.sk>
2025-08-08 22:15:08 +0200

    feat: Add helmet package to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { GraphQLValidationPipe } from './shared/pipe/graphql-validation.pipe';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(new GraphQLValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
