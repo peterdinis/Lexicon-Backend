eaf42f961810a82a62a3f285959f52a642bce6c7
peterdinis <petter1@pobox.sk>
2025-08-12 18:01:06 +0200

    feat: Working on search module

---
+++ src/search/dto/search-result.ts
@@ -0,0 +1,35 @@
+import { ObjectType, Field } from "@nestjs/graphql";
+
+@ObjectType()
+export class UserResult {
+  @Field() id: number;
+  @Field() email: string;
+  @Field({ nullable: true }) name?: string;
+  @Field({ nullable: true }) lastName?: string;
+}
+
+@ObjectType()
+export class PageResult {
+  @Field() id: number;
+  @Field() title: string;
+  @Field({ nullable: true }) emoji?: string;
+}
+
+@ObjectType()
+export class WorkspaceResult {
+  @Field() id: number;
+  @Field() name: string;
+}
+
+@ObjectType()
+export class TaskResult {
+  @Field() id: number;
+  @Field() title: string;
+  @Field({ nullable: true }) description?: string;
+}
+
+@ObjectType()
+export class EventResult {
+  @Field() id: number;
+  @Field() title: string;
+}
