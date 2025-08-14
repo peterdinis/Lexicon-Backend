dae4ec730854e5d98e85e4bbee61927c0035aee0
peterdinis <petter1@pobox.sk>
2025-08-14 11:19:01 +0200

    feat: New method for returning actual workspace

--- src/workspaces/workspaces.resolver.ts
+++ src/workspaces/workspaces.resolver.ts
@@ -8,7 +8,7 @@ import { Workspace } from './workspace.model';
 
 @Resolver(() => Workspace)
 export class WorkspacesResolver {
-  constructor(private readonly workspacesService: WorkspacesService) {}
+  constructor(private readonly workspacesService: WorkspacesService) { }
 
   @Mutation(() => Workspace)
   createWorkspace(@Args('input') input: CreateWorkspaceInput) {
@@ -41,5 +41,12 @@ export class WorkspacesResolver {
     @Args('workspaceId', { type: () => Int }) workspaceId: number,
   ) {
     return this.workspacesService.switchWorkspace(userId, workspaceId);
+  }
+
+  @Query(() => Workspace, { name: 'currentWorkspace' })
+  currentWorkspace(
+    @Args('userId', { type: () => Int }) userId: number,
+  ) {
+    return this.workspacesService.getCurrentWorkspace(userId);
   }
 }
