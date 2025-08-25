6fad4409533ef269da463c6b6cc6395edba2dcc6
peterdinis <petter1@pobox.sk>
2025-08-25 22:19:27 +0200

    feat: Working on notes

---
+++ src/notes/dto/create-note.input.ts
@@ -0,0 +1,17 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { GraphQLJSON } from 'graphql-type-json';
+
+@InputType()
+export class CreateNoteInput {
+  @Field()
+  name: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field(() => GraphQLJSON)
+  content: any;
+
+  @Field()
+  cratedBy: number;
+}
---
+++ src/notes/notes.resolver.ts
@@ -0,0 +1,35 @@
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { CreateNoteInput } from './dto/create-note.input';
+import { UpdateNoteInput } from './dto/update-note.input';
+import { Note } from './model/note.model';
+import { NoteService } from './notes.service';
+
+@Resolver(() => Note)
+export class NoteResolver {
+  constructor(private readonly noteService: NoteService) {}
+
+  @Query(() => [Note], { name: 'notes' })
+  findAll() {
+    return this.noteService.findAll();
+  }
+
+  @Query(() => Note, { name: 'note' })
+  findOne(@Args('id', { type: () => ID }) id: string) {
+    return this.noteService.findOne(id);
+  }
+
+  @Mutation(() => Note)
+  createNote(@Args('input') input: CreateNoteInput) {
+    return this.noteService.create(input);
+  }
+
+  @Mutation(() => Note)
+  updateNote(@Args('input') input: UpdateNoteInput) {
+    return this.noteService.update(input);
+  }
+
+  @Mutation(() => Note)
+  removeNote(@Args('id', { type: () => ID }) id: string) {
+    return this.noteService.remove(id);
+  }
+}
